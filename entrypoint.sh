7bfc2fd7c11971b0b806f25bb9b3056778e0ef4c
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2022-11-23 14:31:55 +0100

    Revert "Add Lodestar capability"

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,10 +17,6 @@ case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_GNOSIS" in
   echo "Using nimbus-gnosis.dnp.dappnode.eth"
   JWT_PATH="/security/nimbus/jwtsecret.hex"
   ;;
-"lodestar-gnosis-dnp.dappnode.eth")
-  echo "Using lodestar-gnosis-dnp.dappnode.eth"
-  JWT_PATH="/security/lodestar/jwtsecret.hex"
-  ;;
 *)
   echo "Using default"
   JWT_PATH="/security/default/jwtsecret.hex"
